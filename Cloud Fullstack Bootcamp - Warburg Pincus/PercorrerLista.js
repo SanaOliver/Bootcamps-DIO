516a845be791068940a744a8d4014fab2fef6e4d
SanaOliver <sanaorihime@gmail.com>
2023-01-03 13:30:46 -0300

    13e30

---
+++ Cloud Fullstack Bootcamp - Warburg Pincus/PercorrerLista.js
@@ -0,0 +1,7 @@
+//Crie um programa que percorra uma lista de numeros e imprima cada numero par encontrado://
+lista = [1,2,3,6,45,87,9,52,13,10,32]
+for (let i = 0; i< lista.length; i++){
+    if(lista[i]%2==0){
+    console.log(lista[i]);
+}
+}
